ab0dbfc87d805a52a2b795d7cfb6f8786ef4da2f
Ryan Santos <rsantos.sjsu@gmail.com>
2014-11-28 09:49:11 -0800

    add working server. twitter file excluding since it has access keys

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+  'twitterAuth' : {
+    'consumerKey'       : 'pAQhCQsDmRv1cxZi8yq9pv3Ps',
+    'consumerSecret'    : 'xWl6ESrmHsQ7AoYjcF4KxUSUwKw6VrEIOStKVolrNWevonDkmP',
+    'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+  }
+
+};
+
---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+
+  'url' : 'mongodb://heroku:lw_Y698E0BD0_jC7GUJixCyWAbR_jUFR6EsaRWz6BbvtcebEeQO3yzN6PwRshDN-Zb5Ep6YCIRgP_KIAUpvFlQ@kahana.mongohq.com:10012/app29477622' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
+
+
+// will hold our database connection settings
